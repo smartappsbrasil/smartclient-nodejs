4c34c60ab1d2c9bd70baf5bd5258cc10533cd154
José Wilker <jose.wilker@smartapps.com.br>
2015-05-07 19:55:52 -0300

    added package.json

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 
 // when load the library you receive 'SMARTAPI' default.
 
-_saApiUrl = "10.211.55.3";
-_saSchema = "a42a3a76e8cb0f228dbb098d6fcf90b0";
-_saConnectKey = "Basic YTQyYTNhNzZlOGNiMGYyMjhkYmIwOThkNmZjZjkwYjA6Q21aVGFGZGxBRGtQWWdWZw==";
+_saApiUrl = "www.smartapps.com.br";
+_saSchema = "6478c499a047234defbd2dcdb489625d";
+_saConnectKey = "Basic ODFiZDkzM2U0NzUzYTQ1MzI5YzJlM2QzMDJjYTMxNTU6V2pZSE9WWm5BRGtOYndsdA==";
 
 require("./smartapi/main.js");
 
@@ -15,25 +15,27 @@ SMARTAPI.connect(init);
 function init(request) {
 	if (request.status == 1) {
 
-		// get schemas
-		SMARTAPI.getSchemas(function(r){
-			console.log(r);
-		});
-
-		// get schemas
-		SMARTAPI.getData('variaveis_valores/_last/1',function(r2){
+		// get data
+		SMARTAPI.getData('variaveis_valores/variavel/37',function(r2){
 			console.log(r2);
 		});
 
-		// send data
-		SMARTAPI.sendExecData('variaveis_valores/insert', 'variavel=4&valor=20', 'json', function(response){
-			console.log(response);
+		// get schemas
+		/*
+		SMARTAPI.getSchemas(function(r){
+			console.log(r);
 		});
 
 		// send data multiple
 		SMARTAPI.sendExecData('variaveis_valores/insert', 'variavel[]=4&valor[]=20&variavel[]=4&valor[]=23&tmd=1', 'json', function(response){
 			console.log(response);
-		});
+		});*/
+
+		// send data
+		/*
+		SMARTAPI.sendExecData('variaveis_valores/insert', 'variavel=4&valor=20', 'json', function(response){
+			console.log(response);
+		});*/
 
 	}
 
